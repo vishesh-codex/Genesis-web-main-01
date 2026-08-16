ec4783238299ec6b86db8d6c815bcb7195531f40
vishesh-codex <158712030+vishesh-codex@users.noreply.github.com>
2026-08-16 14:51:44 +0530

    Add/Update components/ui/navbar.tsx

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from "react"
+import Link from "next/link"
+import Header from "@/components/header"
+
+export function Navbar() {
+  return <Header />
+}
+
+export default Navbar
